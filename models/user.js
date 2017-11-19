7ad6429e4d9c561b1fab97fde4c38f261dd31bde
John Otu <johnne01@gmail.com>
2017-11-19 20:17:19 +0000

    Added User models and routes
    Added chema and methods to create new user and get records of an existing user by their FB Id.
    Added routes t process POST and GET requests for user data.
    Added authentication by Mitman-token for calls to GET and POST routes.
    Changes to be committed:
    	new file:   models/user.js
    	modified:   routes/user.js

---
+++ models/user.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+  first_name: { type: String },
+  last_name: { type: String },
+  fb_id: { type: String },
+  email: { type: String },
+  phone_number: { type: String },
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.addUser = (newUser, callback) => {
+  newUser.save(callback);
+};
+
+module.exports.getUserByFbId = (fbId, callback) => {
+  User.findOne({ fb_id: fbId }, callback);
+};
--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,70 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const User = require('../models/user');
+
+let authenticateCall = (req, res, next) => {
+  if (process.env.MITMAN_TOKEN == req.get('mitman-token')) {
+    return next();
+  }
+  res.status(401).json({
+    success: false,
+    msg: 'Unauthorized request',
+  })
+}
+
+router.get('/', authenticateCall, (req, res) => {
   res.status(200).send('User');
 });
+
+router.post('/', authenticateCall, (req, res) => {
+  let newUser = new User({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    fb_id: req.body.fb_id,
+    email: req.body.email,
+    phone_number: req.body.phone_number,
+  });
+
+  User.addUser(newUser, (err, user) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        msg: 'Error adding user',
+        err: err,
+      });
+    }
+    res.status(201).json({
+      success: true,
+      msg: 'Successfully added user data',
+      user: user,
+    });
+  });
+});
+
+router.get('/:fbid', authenticateCall, (req, res) => {
+  let fbId = req.params.fbid;
+  User.getUserByFbId(fbId, (err, user) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        msg: 'Error getting user data',
+        err: err,
+      });
+    };
+    if (!err && !user) {
+      return res.status(404).json({
+        success: false,
+        msg: 'User not found',
+        err: err,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      msg: 'Successfully obtained user data',
+      user: user,
+    });
+  })
+})
 
 module.exports = router;
